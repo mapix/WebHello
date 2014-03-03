ddfd60441290731f1241fa5093db4b89453fd1b9
luoweifeng <luoweifeng@douban.com>
2014-03-04 01:43:25 +0800

    add for loop support

--- WebHello/template.py
+++ WebHello/template.py
@@ -4,15 +4,27 @@ import re
 from os.path import join
 
 __all__ = ["Lookup", "Template"]
-
-RENDER_BLOCK_RE = re.compile("")
+BLOCK_INVOKE_RE = re.compile("xxx")
 EVA_CONTEXT_RE = re.compile(r'''
-    {
-     (?P<mark>[%#=])
-      (?P<statement>.*)
-     (?P=mark)
-    }''', re.VERBOSE)
-SIGNATURE_RE = re.compile("")
+(
+  {(?P<mark>[\^\$@=#])
+    \s*(?P<statement>.*?)
+  (?P=mark)}
+|
+  {%
+    \s*(?P<director>for)\s+(?P<director_info>.*?)\s*
+  %}
+  (?P<director_body>.*?)
+  {%
+    \s*end(?P=director)\s*
+  %}
+)
+''', re.VERBOSE | re.DOTALL)
+FOR_EVA_RE = re.compile(r'''
+^
+  (?P<item>(?:\w+)(?:\s*,\s*\w+)?)\s*in\s*(?P<items>.*?)
+$
+''', re.VERBOSE | re.DOTALL)
 
 
 class EvaluationContext(object):
@@ -29,7 +41,26 @@ class EvaluationContext(object):
             mark = match.group('mark')
             if mark == "=":
                 statement = match.group('statement').strip()
-                output += eval(statement, self.namespace)
+                output += str(eval(statement, self.namespace))
+            elif mark == "$":
+                statement = match.group('statement').strip()
+                _match = BLOCK_INVOKE_RE.match(statement)
+                block = self.template.blocks[_match.group('block')]
+                _namespace = eval(statement, {k: v for k, v in block.signature.items()})
+                block.render(_namespace)
+            elif not mark:
+                director = match.group('director').strip()
+                director_info = match.group('director_info')
+                director_body = match.group('director_body')
+                if director == "for":
+                    _match = FOR_EVA_RE.match(director_info)
+                    _item = _match.group('item')
+                    items = eval(_match.group('items'), self.namespace)
+                    for item in items:
+                        self.namespace['__WEB_HELLO_FOR_EVA__'] = item
+                        exec "%s = __WEB_HELLO_FOR_EVA__" % _item in self.namespace
+                        del self.namespace['__WEB_HELLO_FOR_EVA__']
+                        output += EvaluationContext(self.template, director_body, self.namespace).render()
             last_pos = match.end()
         return output + self.source[last_pos:]
 
@@ -42,9 +73,9 @@ class RenderBlock(object):
         self.parse_raw_source()
 
     def parse_raw_source(self):
-        name, signature = "", "" #TODO
-        self.name = name
-        self.signature = signature
+        self.name = ""
+        self.signature = {}
+        exec "def %s:return locals()" % self.raw_signature in self.signature
 
     def make_context(self, namespace):
         #TODO filter with signature
--- examples/todolist/views/hello.py
+++ examples/todolist/views/hello.py
@@ -4,5 +4,6 @@ from libs import serve_template
 
 def say_hello(request, name="mapix"):
     action = request.get_var('action', 'eat')
+    environ = request.environ
     return serve_template('hello.html', **locals())
 
